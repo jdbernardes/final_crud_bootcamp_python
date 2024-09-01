620dee8274afcc4404fd77925881263f98725753
Júlio Bernardes <juliocd.bernardes@gmail.com>
2024-09-01 20:47:17 -0300

    (schema/products.py) - Adjusting  pydantic schema

--- backend/schema/products.py
+++ backend/schema/products.py
@@ -1,6 +1,5 @@
-from pydantic import BaseModel, PositiveFloat, EmailStr
-
-# from enum import Enum
+from pydantic import BaseModel, PositiveFloat, EmailStr, validator
+from enum import Enum
 from datetime import datetime
 from typing import Optional
 
@@ -9,6 +8,14 @@ from typing import Optional
 # but in other operations they may not, for example:
 # when creating a product is not needed to provide ID and created at
 # since they are auto generated, now in query they are part of.
+
+
+class CategoriaBase(Enum):
+    category1 = "Eletrônico"
+    category2 = "Eletrodoméstico"
+    category3 = "Móveis"
+    category4 = "Roupas"
+    category5 = "Calçados"
 
 
 class ProductBase(BaseModel):
@@ -37,3 +44,11 @@ class ProductUpdate(ProductBase):
     price: Optional[PositiveFloat] = None
     category: Optional[str] = None
     suplier_email: Optional[EmailStr] = None
+
+    @validator("categoria", pre=True, always=True)
+    def check_categoria(cls, v):
+        if v is None:
+            return v
+        if v in [item.value for item in CategoriaBase]:
+            return v
+        raise ValueError("Categoria inválida")
